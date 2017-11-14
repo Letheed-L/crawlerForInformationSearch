53d4df7c4e1b1778bbdd1c84eb5ff1136290fa7f
Eric Li <ericlxhk@gmail.com>
2017-11-14 21:04:07 +0800

    Update crawlerForInformationSearch.py

--- crawlerForInformationSearch.py
+++ crawlerForInformationSearch.py
@@ -1,11 +1,6 @@
 # -*- coding: utf-8 -*-
-
-# Title: crawlerForQuanshuwang
-
+# Title: crawlerForInformationSearch
 # Author: EricLX
-
 # Operating Environment: Python 2.X
-
 # Version: 1.0.6
-
 # Date: 06 November, 2017
